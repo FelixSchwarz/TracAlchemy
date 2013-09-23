11d9b464a889aab8796b3ba127261c9936af1db8
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2013-09-23 11:00:42 +0200

    map granted permissions to a basic model

---
+++ tracalchemy/permissions.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# 
+# Copyright (c) 2013 Felix Schwarz <felix.schwarz@oss.schwarz.eu>
+#
+# The MIT License
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from sqlalchemy import UnicodeText
+from sqlalchemy.orm import mapper
+from sqlalchemy.schema import Column,  Table
+
+from tracalchemy.model_util import metadata
+
+
+__all__ = ['GrantedPermission']
+
+
+permission_table = Table('permission', metadata,
+    Column('username', UnicodeText, primary_key=True),
+    Column('action', UnicodeText, primary_key=True),
+)
+
+class GrantedPermission(object):
+    @classmethod
+    def query(cls, session):
+        return session.query(cls)
+mapper(GrantedPermission, permission_table)
+
